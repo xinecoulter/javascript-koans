43d5a23df380a967086f6ac2d3863f8f0acb1798
Christine Coulter <christinetcoulter@gmail.com>
2013-07-23 12:11:00 -0400

    Complete 3/53

--- koans/AboutExpects.js
+++ koans/AboutExpects.js
@@ -2,12 +2,12 @@ describe("About Expects", function() {
 
   //We shall contemplate truth by testing reality, via spec expectations.
   it("should expect true", function() {
-    expect(false).toBeTruthy(); //This should be true
+    expect(true).toBeTruthy(); //This should be true
   });
 
   //To understand reality, we must compare our expectations against reality.
   it("should expect equality", function () {
-    var expectedValue = FILL_ME_IN;
+    var expectedValue = 2;
     var actualValue = 1 + 1;
 
     expect(actualValue === expectedValue).toBeTruthy();
@@ -15,7 +15,7 @@ describe("About Expects", function() {
 
   //Some ways of asserting equality are better than others.
   it("should assert equality a better way", function () {
-    var expectedValue = FILL_ME_IN;
+    var expectedValue = 2;
     var actualValue = 1 + 1;
 
   // toEqual() compares using common sense equality.
